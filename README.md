cf01b21f77e3d62eeb29183313a60107e7062630
Dane Randel <nameofdane@gmail.com>
2026-02-25 12:25:37 -0700

    updates

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # numbers-guessing-game
 I'm thinking of a number... try to guess it.
+
+
+## Ideas for improvement
+
+- [ ] Ensure only numbers (only integers) are accepted as input.
+- [ ] Ensure entered numbers are within the valid range.
+- [ ] Include the user's guess in the output messages.
+- [ ] Let the user know how many guesses they have remaining.
--- game.py
+++ game.py
@@ -10,32 +10,29 @@ def greet(min_number, max_number, max_guesses):
 
 
 def play_game(min_number, max_number, max_guesses):
-    # TODO:
     # Choose a random number:
     random_number = random.choice(range(0, max_number))
     
     for attempt in range(0, max_guesses):
+        # TODO: make sure the entered value is an integer.
+        # tip: `if isinstance(guess, int): ... else: ...`
         guess = int(input(f"Enter a number: "))
+
+        # Check if the provided guess is correct.
         if guess == random_number: 
+            # If it is, print a congratulatory message and exit.
             print("You won! The grand and glorious jackpot!")
             sys.exit()
+        # Otherwise, say whether the guess was too low or too high.
         if guess > random_number: 
             print("Your guess is too high")
         if guess < random_number:
             print("Your guess is too low")
-        ##if guess is not int 
-    print("You are not the chosen brother, Eli. You brother Paul is the smart one, and he would try again.")
     
-    
-        # TODO:
-        # Print a message showing the user's guess.
-        # Check if the provided guess is correct.
-        # If it is, print a congratulatory message and exit.
-        # If not, say whether the guess was too low or too high.
-
     # We are now outside of the above for-loop.
     # If we reach this point, it means we have run out of guesses.
     # Insult the user, and invite them to try again (or not).
+    print("You are not the chosen brother, Eli. You brother Paul is the smart one, and he would try again.")
 
 
 # The following line will evaluate to True when the script is executed directly (as opposed to being `import`ed from another python file):
